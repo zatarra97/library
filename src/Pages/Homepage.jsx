5e8c6ea1a82d6d690c7818d57b1bec3d693d7598
MissingTech <missingtech@gmail.com>
2023-02-26 19:34:18 +0100

    Aggiunti nuovi componenti, migliorate pagine

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,42 +1,44 @@
 import Navbar from "../Components/Navbar";
+import Breadcrumb from "../Components/Breadcrumb";
+
 function Homepage(){
     return <>
         <Navbar />
 
-        <div id="indicators-carousel" className="relative mt-16" data-carousel="static">
+        <div id="indicators-carousel" className="relative mt-20" data-carousel="static">
 
-          {/* Carousel wrapper */}
-          <div className="relative h-56 overflow-hidden md:h-96">
-              {/* Item 1 */}
-              <div className="hidden duration-700 ease-in-out" data-carousel-item="active">
-                  <img src="https://picsum.photos/1920/1080" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-              </div>
-              {/* Item 2 */}
-              <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <img src="https://picsum.photos/1920/1080" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-              </div>
-              {/* Item 3 */}
-              <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <img src="https://picsum.photos/1920/1080" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-              </div>
-              {/* Item 4 */}
-              <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <img src="https://picsum.photos/1920/1080" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-              </div>
-              {/* Item 5 */}
-              <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <img src="https://picsum.photos/1920/1080" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-              </div>
-          </div>
+            {/* Carousel wrapper */}
+            <div className="relative h-56 overflow-hidden md:h-96">
+                {/* Item 1 */}
+                <div className="hidden duration-700 ease-in-out" data-carousel-item="active">
+                    <img src="https://picsum.photos/1920/1080" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+                </div>
+                {/* Item 2 */}
+                <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img src="https://picsum.photos/1920/1080" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+                </div>
+                {/* Item 3 */}
+                <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img src="https://picsum.photos/1920/1080" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+                </div>
+                {/* Item 4 */}
+                <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img src="https://picsum.photos/1920/1080" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+                </div>
+                {/* Item 5 */}
+                <div className="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img src="https://picsum.photos/1920/1080" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+                </div>
+            </div>
 
-          {/* Slider indicators */}
-          <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-              <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-              <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-              <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-              <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-              <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-          </div>
+            {/* Slider indicators */}
+            <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+                <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+                <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+                <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+                <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
+                <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
+            </div>
 
           {/* Slider controls */}
           <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
@@ -52,6 +54,10 @@ function Homepage(){
               </span>
           </button>
       </div>
+
+      
+    <Breadcrumb previousPage="Pagina precedente" previousLink="/precedente" currentPage="Pagina corrente" />
+
 
 
       {/*Banner App*/}
--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,6 +1,17 @@
 function NotFound(){
     return <>
-    <h1>Pagina non trovata</h1>
+    <section className="flex items-center h-full p-16 py-60">
+        <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+            <div className="max-w-md text-center">
+                <h2 className="mb-8 font-extrabold text-9xl text-gray-500">
+                    404
+                </h2>
+                <p className="text-2xl font-semibold md:text-3xl">Questa pagina non esiste.</p>
+                <p className="mt-4 mb-8">Ma non preoccuparti, puoi tornare alla homepage da qui.</p>
+                <a href="/" className="cta primary">Torna homepage</a>
+            </div>
+        </div>
+    </section>
     </>
 }
 
